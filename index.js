5c93c1c7743bef7aee9df9a7ad342cfe6fbffe62
Ducr <ducrong@126.com>
2025-10-30 18:03:16 +0800

    feat: 添加TimePickerPro组件导出 (#1)
    添加TimePickerPro组件导出，默认导出DateTimePickerPro组件，保持向后兼容

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+import TimePickerPro from './src/picker/time-picker';
 import DateTimePickerPro from './src/picker/date-picker';
 
 /* istanbul ignore next */
+TimePickerPro.install = function(Vue) {
+  Vue.component(TimePickerPro.name, TimePickerPro);
+};
+/* istanbul ignore next */
 DateTimePickerPro.install = function install(Vue) {
   Vue.component(DateTimePickerPro.name, DateTimePickerPro);
+};
+
+export {
+  TimePickerPro,
+  DateTimePickerPro,
 };
 
 export default DateTimePickerPro;
--- src/picker/time-picker.js
+++ src/picker/time-picker.js
@@ -5,7 +5,7 @@ import TimeRangePanel from '../panel/time-range';
 export default {
   mixins: [Picker],
 
-  name: 'ElTimePicker',
+  name: 'TimePickerPro',
 
   props: {
     isRange: Boolean,
